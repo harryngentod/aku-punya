4ce8bcc1c4cfada3395dbdce63278d1f14a312b3
harryngentod <104566310+harryngentod@users.noreply.github.com>
2022-04-28 19:27:43 +0800

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,13 @@
+# Kang Spam
+```
+Tools ini dibuat untuk ngerjain
+Kang Ripper atau boleh juga buat
+nyepam Mantan Lu awokawokwkwk:v
+```
+![Sanz](https://github.com/Sxp-ID/Kang-Spam/blob/main/.Tools%20Kang%20Spam%20by%20Sanz.png)
+> Script ini sewaktu-waktu bisa jadi limit ataupun coid jadi jangan salahin author nya ya goblok.
+## How to it?
+```python
+$ cd Kang-Spam
+$ bash install.sh
+```
